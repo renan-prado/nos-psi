52bd00f1135f8688cd01eaa670761a38b3dabd37
Renan Prado <renanprado96@gmail.com>
2025-09-09 17:06:08 -0300

    conteudo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { Logo } from '@/components'
 import { WhatsappIcon } from '@/components/icons'
 import { MenuMain } from '@/components/navigation'
 import { SidebarTrigger } from '@/components/ui/sidebar'
+import Image from 'next/image'
 import Link from 'next/link'
 
 export default function Home() {
@@ -24,6 +25,38 @@ export default function Home() {
           <SidebarTrigger className="flex sm:hidden" />
         </div>
       </header>
+      <section className="relative pl-10 w-full h-[420px] flex items-center justify-between">
+        <div className="w-full -z-10 h-full absolute top-0 left-0">
+          <Image
+            width={1440}
+            height={585}
+            src="/banner-main.png"
+            alt="Quote"
+            className="w-full h-full object-cover object-top"
+          />
+        </div>
+        <h2 className="text-4xl leading-12 max-w-lg">
+          “Todo mundo carrega nós. Aqui, você encontra um lugar para desatar,
+          compreender e se reconectar”
+        </h2>
+        <div className="bg-primary rounded-l-4xl overflow-hidden">
+          <Image
+            width={1440}
+            height={585}
+            src="/dupla-01.png"
+            alt="Dupla"
+            className="size-84 object-cover grayscale opacity-75"
+          />
+        </div>
+      </section>
+      <section className="px-6 bg-primary text-secondary-lightest py-20 flex items-center justify-center">
+        <p className="text-lg leading-8 max-w-md text-center font-light">
+          Na NósPsi, acreditamos no poder do encontro: entre você e sua
+          história, entre o cuidado e a escuta, entre o que te atravessa e o que
+          você deseja transformar.Atendemos adolescentes e adultos, com sessões
+          presenciais em São Paulo ou online.
+        </p>
+      </section>
     </>
   )
 }
